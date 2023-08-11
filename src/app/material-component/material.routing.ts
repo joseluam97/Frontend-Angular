3788028f15d18dde261d6b06911457cc4d9cd78d
Jose Luis Astorga Morillo <joseluastorga97@gmail.com>
2023-08-11 18:49:20 +0200

    Delete src directory

--- src/app/material-component/material.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { MIsFavoritosComponent } from './mis-favoritos/mis-favoritos.component';
-
-export const MaterialRoutes: Routes = [
-  {
-    path: 'mis-favoritos',
-    component: MIsFavoritosComponent
-  }
-];
